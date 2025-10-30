f72a6303e3c0b9eb279e40d49ce1ba23accb27b8
ibugueno <ignacio.bugueno@live.com>
2025-10-30 19:11:44 +0100

    increasing shm memory

--- run_docker.sh
+++ run_docker.sh
@@ -2,4 +2,11 @@
 
 docker rm -fv ignacio_event_reconst_pix2pix
 
-docker run -it --gpus '"device=6,7"' --name ignacio_event_reconst_pix2pix -v /home/ignacio.bugueno/cachefs/event_reconst_pix2pix/input:/app/input -v /home/ignacio.bugueno/cachefs/event_reconst_pix2pix/output:/app/output ignacio_event_reconst_pix2pix
+docker run -it \
+  --gpus '"device=0,1"' \
+  --name ignacio_event_reconst_pix2pix \
+  --shm-size=32g \
+  --ipc=host \
+  -v /home/ignacio.bugueno/cachefs/event_reconst_pix2pix/input:/app/input \
+  -v /home/ignacio.bugueno/cachefs/event_reconst_pix2pix/output:/app/output \
+  ignacio_event_reconst_pix2pix
